4e62ec0b68c04e8c4e50f0e6a6ffa555be7b2800
maronski <maronbalinas@gmail.com>
2026-02-11 17:19:31 +0800

    chore(frontend): bootstrap vite react typescript app

---
+++ frontend/vite.config.ts
@@ -0,0 +1,20 @@
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import tailwindPostcss from "@tailwindcss/postcss";
+import autoprefixer from "autoprefixer";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  css: {
+    postcss: {
+      plugins: [tailwindPostcss(), autoprefixer()]
+    }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
+});
